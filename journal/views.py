bcd455b2f9db09ce5484b65c64d010e3e82d8fac
Andrew McMahon <andrew.mcmahon@solirius.com>
2021-09-07 16:54:14 +0100

    Working App with HTML Template

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,16 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from journal.models import Resource
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    output = "Hello World"
+    resources = Resource.objects.all()
+    template = loader.get_template('resources.html')
+    resource_list = ", ".join([r.name for r in resources])
+    context = {
+        'resources': resources,
+    }
+    return HttpResponse(template.render(context, request))
--- learnjournal/urls.py
+++ learnjournal/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import journal
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('journal/', include('journal.urls')),
+    path('', include('journal.urls')),
 ]
