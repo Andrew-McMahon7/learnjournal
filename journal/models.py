45d05c87a936dc9cd69ba3ad4d943271f280c78f
Andrew McMahon <andrew.mcmahon@solirius.com>
2021-09-08 15:41:15 +0100

    Setup DB

--- journal/models.py
+++ journal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
@@ -6,3 +7,23 @@ from django.db import models
 class Resource(models.Model):
     name = models.CharField(max_length=200)
     url = models.URLField()
+
+
+class JournalResource(models.Model):
+    journalName = models.CharField(max_length=200)
+    journalUrl = models.URLField()
+    lastAccessed = models.DateTimeField()
+    tagIds = models.JSONField()
+
+
+class TagsResource(models.Model):
+    tagName = models.CharField(max_length=200)
+    tagUrl = models.URLField()
+    imageUrl = models.URLField(blank = True)
+
+
+class ContactsResource(models.Model):
+    contactName = models.CharField(max_length=200)
+    contactEmail = models.EmailField(blank = True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    contactNumber = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
