384d4422f1ee1c3cbc9b1fb11e37ade8e37a0e11
Andrew McMahon <andrew.mcmahon@solirius.com>
2021-09-09 11:49:51 +0100

    Updated Tags Resources

--- journal/models.py
+++ journal/models.py
@@ -17,18 +17,23 @@ class TagsResource(models.Model):
     def __str__(self):
         return self.tagName
 
-class JournalResource(models.Model):
-    journalName = models.CharField(max_length=200)
-    journalUrl = models.URLField()
-    lastAccessed = models.DateTimeField()
-    tagIds = models.ManyToManyField(TagsResource)
-
-
-
 
 class ContactsResource(models.Model):
     contactName = models.CharField(max_length=200)
     contactEmail = models.EmailField(blank = True)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     contactNumber = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+
+    def __str__(self):
+        return self.contactName
+
+
+class JournalResource(models.Model):
+    journalName = models.CharField(max_length=200)
+    journalUrl = models.URLField()
+    lastAccessed = models.DateTimeField()
+    tagNames = models.ManyToManyField(TagsResource)
+    contactNames = models.ManyToManyField(ContactsResource)
+
+
 
